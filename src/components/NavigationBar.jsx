f9a6c3f2d8b768b802ada92a90205ef6553d590b
Scott12141 <12141@coderacademy.edu.au>
2023-12-21 18:10:08 +1100

    toned down red on headers

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,28 +5,28 @@ export default function NavBar(props){
         <nav className="nav">
 
                 <li>
-                    <NavLink to="/" style={({isActive}) => isActive ? {color: "rgb(201, 31, 31)", textDecoration: 'none' } : undefined}>
+                    <NavLink to="/" style={({isActive}) => isActive ? {color: "rgb(175, 49, 49)", textDecoration: 'none' } : undefined}>
                         Home
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/dining" style={({isActive}) => isActive ? {color: "rgb(201, 31, 31)", textDecoration: 'none' } : undefined}>
+                    <NavLink to="/dining" style={({isActive}) => isActive ? {color: "rgb(175, 49, 49)", textDecoration: 'none' } : undefined}>
                         Dining
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/beer" style={({isActive}) => isActive ? {color: "rgb(201, 31, 31)", textDecoration: 'none' } : undefined}>
+                    <NavLink to="/beer" style={({isActive}) => isActive ? {color: "rgb(175, 49, 49)", textDecoration: 'none' } : undefined}>
                         Beer
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/bookings" style={({isActive}) => isActive ? {color: "rgb(201, 31, 31)", textDecoration: 'none' } : undefined}>
+                    <NavLink to="/bookings" style={({isActive}) => isActive ? {color: "rgb(175, 49, 49)", textDecoration: 'none' } : undefined}>
                         Bookings
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/users" style={({isActive}) => isActive ? {color: "rgb(201, 31, 31)", textDecoration: 'none' } : undefined}>
+                    <NavLink to="/users" style={({isActive}) => isActive ? {color: "rgb(175, 49, 49)", textDecoration: 'none' } : undefined}>
                         Log In
                     </NavLink>
                 </li>
